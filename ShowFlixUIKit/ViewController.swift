b99ceed89ec7779cef250bfd018772b442de91d8
Mac Mini <>
2024-01-02 14:01:44 +0600

    Project Created

---
+++ ShowFlixUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShowFlixUIKit
+//
+//  Created by mac 2019 on 1/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
