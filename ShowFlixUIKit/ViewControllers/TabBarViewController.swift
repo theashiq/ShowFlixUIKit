cbf605e3fe9036c205e84b16d7114919063070ab
Mac Mini <>
2024-01-03 13:41:59 +0600

    Downloads View Controller

--- ShowFlixUIKit/ViewControllers/TabBarViewController.swift
+++ ShowFlixUIKit/ViewControllers/TabBarViewController.swift
@@ -16,7 +16,7 @@ class TabBarViewController: UITabBarController {
         let vc1 = UINavigationController(rootViewController: HomeViewController())
         let vc2 = UINavigationController(rootViewController: UpcomingViewController())
         let vc3 = UINavigationController(rootViewController: SearchViewController())
-        let vc4 = UINavigationController(rootViewController: SecondViewController())
+        let vc4 = UINavigationController(rootViewController: DownloadsViewController())
     
         setViewControllers([vc1, vc2, vc3, vc4], animated: true)
         
@@ -28,26 +28,11 @@ class TabBarViewController: UITabBarController {
         vc1.title = HomeViewController.title
         vc2.title = UpcomingViewController.title
         vc3.title = SearchViewController.title
-        vc4.title = SecondViewController.title
+        vc4.title = DownloadsViewController.title
         
         tabBar.tintColor = .label
         tabBar.backgroundColor = .secondarySystemBackground
     }
 }
-
-class SecondViewController: UIViewController{
-    static let title: String = "Second View"
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .green
-        let label = UILabel(frame: view.bounds)
-        label.text = "Second View"
-        label.font = .systemFont(ofSize: 50, weight: .heavy)
-        label.textAlignment = .center
-        view.addSubview(label)
-    }
-}
-
 
 
--- ShowFlixUIKit/ViewModels/ShowViewModel.swift
+++ ShowFlixUIKit/ViewModels/ShowViewModel.swift
@@ -10,11 +10,13 @@ import Foundation
 struct ShowViewModel{
     let title: String
     let posterUrl: URL?
+    let show: Show
     
     static func get(from show: Show) -> ShowViewModel{
         return ShowViewModel(
             title: show.original_name ?? show.original_title ?? "Unknown Show Title",
-            posterUrl: URL(string: "\(Constants.tmdbPosterBaseURL)/\(show.poster_path ?? "")")
+            posterUrl: URL(string: "\(Constants.tmdbPosterBaseURL)/\(show.poster_path ?? "")"),
+            show: show
         )
     }
 }
